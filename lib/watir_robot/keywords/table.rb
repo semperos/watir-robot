be9a0420db60556ab3345388b426b1d01253a46c
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2010-12-27 19:47:34 -0500

    Make keywords for table api 1-based instead of 0-based and add RF tests

--- lib/watir_robot/keywords/table.rb
+++ lib/watir_robot/keywords/table.rb
@@ -11,12 +11,14 @@ module WatirRobot
     # Get contents of a single table cell
     #
     # @param [String] loc attribute/value pairs that match an HTML element
-    # @param [String] row with the top-most row as 0, the number of the row in question
-    # @param [String] col with the left-most row as 0, the number of the column in question
+    # @param [String] row with the top-most row as 1, the number of the row in question
+    # @param [String] col with the left-most row as 1, the number of the column in question
     # @return [String] the text contained within the targeted table cell
     # 
     def get_table_cell(loc, row, col)
-      @browser.table(parse_location(loc))[row.to_i][col.to_i].text
+      row = row.to_i - 1
+      col = col.to_i - 1
+      @browser.table(parse_location(loc))[row][col].text
     end
     
     
@@ -26,29 +28,32 @@ module WatirRobot
     # Verify that table cell contains certain contents
     #
     # @param [String] loc attribute/value pairs that match an HTML element
-    # @param [String] row with the top-most row as 0, the number of the row in question
-    # @param [String] col with the left-most row as 0, the number of the column in question
+    # @param [String] row with the top-most row as 1, the number of the row in question
+    # @param [String] col with the left-most row as 1, the number of the column in question
     # @param [String] text the text to compare against
     # 
     def table_cell_should_contain(loc, row, col, text)
+      row = row.to_i - 1
+      col = col.to_i - 1
       raise(ElementMatchError, "The table cell at row #{row} and column #{col}, located in the table at #{loc}, does not contain the text #{text}") unless
-        @browser.table(parse_location(loc))[row.to_i][col.to_i].text.include? text
+        @browser.table(parse_location(loc))[row][col].text.include? text
     end
     
     #
     # Verify that a table column contains certain contents
     #
     # @param [String] loc attribute/value pairs that match an HTML element
-    # @param [String] col with the left-most row as 0, the number of the column in question
+    # @param [String] col with the left-most row as 1, the number of the column in question
     # @param [String] text the text to compare against
     # 
     def table_column_should_contain(loc, col, text)
       # There's no "nice" way to do this, so we run through each row and snag
       #   the cell at offset y
+      col = col.to_i - 1
       content = ''
       @browser.table(parse_location(loc)).rows.each do |row|
         content << ' '
-        content << row[col.to_i].text
+        content << row[col].text
       end
       
       raise(Exception::ElementMatchError, "The table column number #{c} in the table located at #{loc} does not contain the text #{text}") unless
@@ -70,10 +75,11 @@ module WatirRobot
     # Verify that a table row conatins certain contents
     #
     # @param [String] loc attribute/value pairs that match an HTML element
-    # @param [String] row with the top-most row as 0, the number of the row in question
+    # @param [String] row with the top-most row as 1, the number of the row in question
     # @param [String] text the text to compare against
     # 
     def table_row_should_contain(loc, row, text)
+      row = row.to_i - 1
       raise(Exception::ElementMatchError, "The table row number #{row} in the table located at #{loc} does not contain the text #{text}") unless
         @browser.table(parse_location(loc))[row].text.include? text
     end
