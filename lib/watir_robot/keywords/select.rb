42f643e1e5b9a52ebda5b0ec4dd54fd39af2b48a
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2010-12-27 19:33:09 -0500

    Fix keywords for select lists and add RF test

--- lib/watir_robot/keywords/select.rb
+++ lib/watir_robot/keywords/select.rb
@@ -14,17 +14,17 @@ module WatirRobot
     # @param [String] item the label, text or value of an item to select
     # @param [String] by_value boolean, whether or not to select an element by visible text or by value attribute (as string because arg comes from Robot Framework)
     # 
-    def select_item_from_list(loc, item, by_value = 'false')
+    def select_item_from_list(list_loc, item_loc)
       # Give user option to select by value rather than by text of option or label
       # Since the text is immediately visible, I assume people will default to that
-      by_value = by_value.downcase
-      if value == 'false'
-        @browser.select(parse_locatoin(loc)).select(item)
-      elsif by_value == 'true'
-        @browser.select(parse_location(loc)).select_value(item)
+      if item_loc[0..4].downcase == 'text='
+        item_loc = item_loc[5..item_loc.length]
+        @browser.select(parse_location(list_loc)).select(item_loc)
+      elsif item_loc[0..5].downcase == 'value='
+        item_loc = item_loc[6..item_loc.length]
+        @browser.select(parse_location(list_loc)).select_value(item_loc)
       else
-        raise(ArgumentError,
-              "If you specify that you want to select the list item by its value attribute, the only valid values are 'true' or 'false' (case-insensitive)")
+        raise(ArgumentError, "The only attributes allowed are 'text' and 'value'")
       end
     end
     
@@ -48,10 +48,35 @@ module WatirRobot
     #
     # @param [String] loc attribute/value pairs that match an HTML element
     # 
-    def clear_items_from_select_list(loc)
+    def clear_items_from_list(loc)
       @browser.select(parse_location(loc)).clear
     end
-    
+
+
+    ### Conditions ###
+
+    #
+    # Ensure that an item is selected
+    #
+    # @param [String] list_loc attribute/value pairs that match a select list
+    # @param [String] item_loc attribute/value pairs that match an item in a select list
+    #
+    def item_should_be_selected(list_loc, item_loc)
+      raise(Exception::SelectListSelectionError, "The item described by #{item_loc} in the select list described by #{list_loc} is not selected") unless
+        @browser.select(parse_location(list_loc)).option(parse_location(item_loc)).selected?
+    end
+
+    #
+    # Ensure that an item is not selected
+    #
+    # @param [String] list_loc attribute/value pairs that match a select list
+    # @param [String] item_loc attribute/value pairs that match an item in a select list
+    #
+    def item_should_not_be_selected(list_loc, item_loc)
+      raise(Exception::SelectListSelectionError, "The item described by #{item_loc} in the select list described by #{list_loc} is selected") if
+        @browser.select(parse_location(list_loc)).option(parse_location(item_loc)).selected?
+    end
+
   end
   
 end
